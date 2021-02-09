8bcf02f273d434d670c3806d500b98f1d8f366b2
Youssef Abakhar <yabakhar@e1r4p5.1337.ma>
2021-02-09 19:38:11 +0100

    add . autocomlition 25%

--- SRC_HEAD/readline/auto_complition.c
+++ SRC_HEAD/readline/auto_complition.c
@@ -12,26 +12,7 @@
 
 #include "../includes/sh.h"
 
-int	ft_tab_checker(int index,char *str)
-{
-	int flag = 0;
-	int kk = 0;
-	if (ft_isalpha(str[index]))
-		flag = 1;
-	if (!ft_isalpha(str[index]) && str[index] != ' ')
-		kk = 1;
-	while(index >= 0)
-	{
-		if (!ft_isalpha(str[index]))
-			flag = 0;
-		else if (ft_isalpha(str[index]) && !flag)
-			return (1);
-		else if (!ft_isalpha(str[index]))
-			return(0);
-		index--;
-	}
-	return (0);	
-}
+
 
 
 
@@ -53,7 +34,7 @@ int	ft_tab_checker(int index,char *str)
 // 	// ft_putnbr_fd(xxxxc(c_len,str),open("/dev/ttys001",O_RDWR));
 // }
 
-int ft_pppp(int index,char *str)
+int ft_ispace(int index,char *str)
 {
 	int flag = 0;
 	while (str[index])
@@ -63,27 +44,64 @@ int ft_pppp(int index,char *str)
 		else if (ft_isalnum(str[index]) && !flag)
 			flag = 1;
 		else if (!ft_isalnum(str[index]) && str[index] != ' ')
-			return (1);
-		if (ft_isalnum(str[index]) && flag == 2)
 			return (0);
+		if (ft_isalnum(str[index]) && flag == 2)
+			return (1);
 		index--;
 	}
-	return (1);
+	return (0);
 }
 
+int	ft_tab_checker(int index,char *str)
+{
+	int flag = 0;
+	while(str[index])
+	{
+		if (ft_isalnum(str[index]) && flag)
+			return (1);
+		else if (str[index] == ' ' && !flag)
+			flag = 1;
+		else if (ft_strchr("&|;",str[index]))
+			return(0);
+		index--;
+	}
+	return (0);	
+}
+
+int ft_ciparateur(int index,char *str)
+{
+	int flag = 0;
+	while (str[index])
+	{
+		if (ft_strchr("&|;",str[index]) && flag)
+			return (0);
+		else if (str[index] == ' ' && !flag)
+			flag = 1;
+		else if (ft_isalnum(str[index]))
+			return (1);
+		index--;
+	}
+	return (0);
+}
 
 void  ft_parce_complition(t_line *line, char **str)
 {
 	int r = 0;
-	// if (ft_isalnum(*str[line->c_len]))
-	// {
-	// 	if (ft_tab_checker(line->c_len,*str))
-	// 		ft_putnbr_fd(1,open("/dev/ttys002",O_RDWR));
-	// }
-	
-	if ((*str)[line->c_len] == ' ')
+	if ((*str)[line->c_len] == 0)
 	{
-		ft_putnbr_fd(ft_pppp(line->c_len,*str),open("/dev/ttys002",O_RDWR));
+		ft_putnbr_fd(820,open("/dev/ttys002",O_RDWR));
+	}
+	else if (ft_isalnum((*str)[line->c_len]))
+	{
+		ft_putnbr_fd(ft_tab_checker(line->c_len,*str),open("/dev/ttys002",O_RDWR));
+	}
+	else if ((*str)[line->c_len] == ' ')
+	{
+		ft_putnbr_fd(ft_ispace(line->c_len,*str),open("/dev/ttys002",O_RDWR));
+	}
+	else if (ft_strchr("&|;",(*str)[line->c_len]))
+	{
+		ft_putnbr_fd(ft_ciparateur(line->c_len,*str),open("/dev/ttys002",O_RDWR));
 	}
 }
 
@@ -91,3 +109,6 @@ void ft_auto_complition(t_line *line, char **str)
 {
 	ft_parce_complition(line, str);
 }
+
+
+// if (ft_isalnum())
