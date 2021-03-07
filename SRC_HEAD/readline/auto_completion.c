ef3744e61967e23fec03c7555391aa7fe99d1017
Youssef Abakhar <yabakhar@e1r4p3.1337.ma>
2021-03-07 19:16:56 +0100

    add . autocomlition 85%...

--- SRC_HEAD/readline/auto_completion.c
+++ SRC_HEAD/readline/auto_completion.c
@@ -274,9 +274,9 @@ void make_path_completion(t_line *line,char **str)
 	
 	if (!line->compl.type)
 	{
-		ft_putendl_fd(line->compl.str,open("/dev/ttys002",O_RDWR));
-		// make_path_file(line,1);
-		// ft_get_all_bin_dirs(line,str);
+		// ft_putendl_fd(line->compl.str,open("/dev/ttys002",O_RDWR));
+		make_path_file(line,1);
+		ft_get_all_bin_dirs(line,str);
 
 	}
 	// else if (line->compl.type == 1)
